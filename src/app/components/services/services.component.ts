4b4bfe9ebcab1587dccb5877851c3de227db261f
joshqinbek-github <joshdeveloper22@gmail.com>
2021-02-02 16:49:06 +0500

    done

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {gsap} from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+
+
+gsap.registerPlugin(ScrollTrigger)
 
 @Component({
   selector: 'app-services',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
+  @ViewChild("sect_6", {static: true}) sect_6: ElementRef<HTMLDivElement>;
+
+
   constructor() { }
 
   ngOnInit(): void {
+    // this.scrollAnim();
+    for(let i; i <= this.sect_6.nativeElement.childNodes.length; i++){
+      console.log(i)
+    }
   }
 
+
+
+  scrollAnim():void{
+    gsap.from(this.sect_6.nativeElement.childNodes[1], {
+      scrollTrigger: {
+        trigger: this.sect_6.nativeElement,
+        scrub: true,
+        markers: false,
+        start: "300px top",
+      },
+      duration: 1.5,
+      y: -250,
+      stagger: .4,
+    });
+  }
 }
