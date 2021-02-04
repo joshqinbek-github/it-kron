199c43cea704e90d2062aafd58bd33aafdecb1d9
joshqinbek-github <joshdeveloper22@gmail.com>
2021-02-04 15:53:23 +0500

    da

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -14,6 +14,10 @@ export class ServicesComponent implements OnInit {
 
   @ViewChild("sect_6", {static: true}) sect_6: ElementRef<HTMLDivElement>;
   @ViewChild("sect_5", {static: true}) sect_5: ElementRef<HTMLDivElement>;
+  @ViewChild("sect_4", {static: true}) sect_4: ElementRef<HTMLDivElement>;
+  @ViewChild("sect_3", {static: true}) sect_3: ElementRef<HTMLDivElement>;
+  @ViewChild("sect_2", {static: true}) sect_2: ElementRef<HTMLDivElement>;
+  @ViewChild("sect_1", {static: true}) sect_1: ElementRef<HTMLDivElement>;
 
 
   constructor() { }
@@ -33,12 +37,38 @@ export class ServicesComponent implements OnInit {
     });
   
     gsap.from(this.sect_5.nativeElement.childNodes, {      
-      duration: 1,
-      opacity: 1,
+      duration: 1.5,
+      opacity: 0,
+      y: 150,
       stagger: .4,
     });
 
+      gsap.from(this.sect_4.nativeElement.childNodes, {      
+      duration: 1.5,
+      opacity: 0,
+      y: 150,
+      stagger: .4,
+    });
 
+    gsap.from(this.sect_3.nativeElement.childNodes, {      
+      duration: 1.5,
+      opacity: 0,
+      y: 250,
+      stagger: .4,
+    });
+    gsap.from(this.sect_2.nativeElement.childNodes, {      
+      duration: 1.5,
+      opacity: 0,
+      y: 250,
+      stagger: .4,
+    });
+    gsap.from(this.sect_1.nativeElement.childNodes, {      
+      duration: 1.5,
+      opacity: 0,
+      y: 250,
+      stagger: .4,
+    });
+   
     gsap.to(this.sect_6.nativeElement.childNodes[1].childNodes, {
       delay: 2.2,
       duration: 1,
